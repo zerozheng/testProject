a46cc0c79c23ae30d6cbb0cc30fc54d56940288f
zerozheng <641486122@qq.com>
2017-04-26 22:38:11 +0000

    modify welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello Laravel 5! zerozheng</div>
+                <div class="title">Hello Laravel! zerozheng</div>
             </div>
         </div>
     </body>
